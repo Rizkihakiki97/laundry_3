54df07dac1dced2c952e2125f29d80c061bd043b
ebenhaiser <ebenhaiser@gmail.com>
2024-12-02 16:32:50 +0700

    laporan transaksi

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "ujikom2_laundry";
+
+$koneksi = mysqli_connect($host, $user, $password, $db);
+
+if (empty($koneksi)) {
+    echo "ERROR BRO....";
+}
